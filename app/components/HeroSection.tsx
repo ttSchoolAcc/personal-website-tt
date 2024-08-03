44397a49361a498fcd03fc35ea85947dc67dbb8b
Butter Man40 <ramiro.santosf40@gmail.com>
2024-08-03 16:19:50 -0500

    Added apos

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "}</span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I&apos;m{" "}</span>
             <br></br>
             <TypeAnimation
                 sequence={[
