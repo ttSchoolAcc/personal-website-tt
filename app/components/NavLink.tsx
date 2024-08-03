1f33d365db2b0fff765c88866b6ed84b7ca949dd
Butter Man40 <ramiro.santosf40@gmail.com>
2024-08-03 16:03:54 -0500

    Website

---
+++ app/components/NavLink.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const NavLink = ({href, title}: any) => {
+return (
+    <Link href={href} 
+                className="block py-2 pl-3 pr-4 text-[#ABD7BE] sm:text-xl rounded md:p-0 hover:text-white">
+        {title}
+    </Link>
+)
+}
+
+export default NavLink;
