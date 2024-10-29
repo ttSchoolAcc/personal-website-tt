f3c179761081770ce34ba35613200efe59374309
av23dev <177713416+av23dev@users.noreply.github.com>
2024-10-28 20:50:55 -0500

    fixed prefix error

--- app/components/AchievementsSection.tsx
+++ app/components/AchievementsSection.tsx
@@ -41,7 +41,7 @@ const AchievementsSection = () => {
               className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
             >
               <h2 className="text-white text-4xl font-bold flex flex-row">
-                {achievement.prefix}
+                {/* {achievement.prefix} */}
                 <AnimatedNumbers
                   includeComma
                   animateToNumber={parseInt(achievement.value)}
