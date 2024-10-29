3d3df2836746f66ab84d84c874388273829c2a46
av23dev <177713416+av23dev@users.noreply.github.com>
2024-10-28 20:58:13 -0500

    robots txt

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Contact from "./components/Contact";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <meta name="robots" content="noindex"></meta>
       <Navbar/>
       <div className="container mx-auto px-12 py-4">
         <HeroSection/>
